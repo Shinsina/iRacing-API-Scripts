1fd509c5104909f33be0011257d952663c31d2cb
Shinsina <shinsina@att.net>
2024-05-01 14:04:17 -0500

    Remove flatten-subsessions-output.js (Unused)

--- utils/flatten-subsessions-output.js
+++
@@ -1,7 +0,0 @@
-const output = require("./3-29-2024-subsessions-output.json");
-const fs = require("fs");
-
-const test = Object.keys(output)
-  .map((v) => output[v])
-  .flat();
-fs.writeFileSync("./flattened-subsessions-output.json", JSON.stringify(test));
