d07ae756c71b06a024ac01857926d95704822a9e
Shinsina <shinsina@att.net>
2024-05-01 14:37:05 -0500

    Replace generate-standings-input.js with generate-standings-input.py

---
+++ utils/generate-standings-input.py
@@ -0,0 +1,16 @@
+import json
+
+subsessions = []
+with open('../search-series-output.json', 'r') as input_file:
+  json_data = json.load(input_file)
+  for key in json_data.keys():
+    subsessions.extend(json_data[key])
+
+unique_season_car_class_id_mappings = set()
+for subsession in subsessions:
+  mapping = str(subsession['season_id']) + '_' + str(subsession['car_class_id'])
+  if mapping not in unique_season_car_class_id_mappings:
+    unique_season_car_class_id_mappings.add(mapping)
+
+with open('standings-input.json', 'w') as output_file:
+  json.dump(list(unique_season_car_class_id_mappings), output_file)
