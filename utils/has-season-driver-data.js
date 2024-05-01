8466e10f2ff449adf7c313a885af20be3f4d8af9
Shinsina <shinsina@att.net>
2024-05-01 14:44:50 -0500

    Remove has-season-driver-data.js (Unused)

--- utils/has-season-driver-data.js
+++
@@ -1,4 +0,0 @@
-const output = require('../jack-standings-output.json');
-const fs = require('fs');
-
-fs.writeFileSync('./jack-standings-output.json', JSON.stringify(output.filter((v) => v.season_driver_data)));
