b9432d9237c21d187102984db379996691149386
Shinsina <shinsina@att.net>
2024-08-15 14:46:04 -0500

    Create search-series main.go

---
+++ search-series/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+type CustomerIDSeasonQuarterSeasonYearMapping struct {
+	Customer_ID    int `json:"customer_id"`
+	Season_Quarter int `json:"season_quarter"`
+	Season_Year    int `json:"season_year"`
+}
+
+type iRacingAPIResponse struct {
+	Data struct {
+		Success    bool `json:"success"`
+		Chunk_Info struct {
+			Chunk_Size        int      `json:"chunk_size"`
+			Num_Chunks        int      `json:"num_chunks"`
+			Rows              int      `json:"rows"`
+			Base_Download_Url string   `json:"base_download_url"`
+			Chunk_File_Names  []string `json:"chunk_file_names"`
+		} `json:"chunk_info"`
+	} `json:"data"`
+}
+
+type InitialResponseGrouping struct {
+	customer_id int
+	outer_index int
+	inner_index int
+	response    iRacingAPIResponse
+}
+
+type ChunkResponseGrouping struct {
+	customer_id int
+	outer_index int
+	response    string
+}
+
+func main() {
+
+	content, err := os.ReadFile("../cookie.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var cookies map[string]string
+	err = json.Unmarshal(content, &cookies)
+	if err != nil {
+		fmt.Println(err)
+	}
+	raw_array_of_arrays, err := os.ReadFile("./customer-id-season-quarter-season-year-mappings.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var customer_id_season_quarter_season_year_array_of_arrays [][]CustomerIDSeasonQuarterSeasonYearMapping
+	err = json.Unmarshal(raw_array_of_arrays, &customer_id_season_quarter_season_year_array_of_arrays)
+	if err != nil {
+		fmt.Println(err)
+	}
+	channel_count := 0
+	for i := 0; i < len(customer_id_season_quarter_season_year_array_of_arrays); i++ {
+		channel_count += len(customer_id_season_quarter_season_year_array_of_arrays[i])
+	}
+	channel := make(chan InitialResponseGrouping, channel_count)
+	for i := 0; i < len(customer_id_season_quarter_season_year_array_of_arrays); i++ {
+		for j := 0; j < len(customer_id_season_quarter_season_year_array_of_arrays[i]); j++ {
+			mapping := customer_id_season_quarter_season_year_array_of_arrays[i][j]
+			url := fmt.Sprintf("https://members-ng.iracing.com/data/results/search_series?season_quarter=%d&season_year=%d&cust_id=%d&official_only=true&event_types=5", mapping.Season_Quarter, mapping.Season_Year, mapping.Customer_ID)
+			go func() {
+				req, err := http.NewRequest("GET", url, nil)
+				if err != nil {
+					fmt.Println(err)
+				}
+				for key, value := range cookies {
+					req.AddCookie(&http.Cookie{Name: key, Value: value})
+				}
+				http_client := &http.Client{}
+				resp, err := http_client.Do(req)
+				if err != nil {
+					fmt.Println(err)
+				}
+				defer resp.Body.Close()
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					fmt.Println(err)
+				}
+				var iracing_api_response iRacingAPIResponse
+				err = json.Unmarshal(body, &iracing_api_response)
+				if err != nil {
+					fmt.Println(err)
+				}
+				var channel_output InitialResponseGrouping
+				channel_output.customer_id = mapping.Customer_ID
+				channel_output.outer_index = i
+				channel_output.inner_index = j
+				channel_output.response = iracing_api_response
+				channel <- channel_output
+			}()
+		}
+	}
+	chunk_channel := make(chan ChunkResponseGrouping, channel_count)
+	for i := 0; i < channel_count; i++ {
+		channel_reponse := <-channel
+		customer_id := channel_reponse.customer_id
+		outer_index := channel_reponse.outer_index
+		base_url := channel_reponse.response.Data.Chunk_Info.Base_Download_Url
+		for i := 0; i < len(channel_reponse.response.Data.Chunk_Info.Chunk_File_Names); i++ {
+			url := base_url + channel_reponse.response.Data.Chunk_Info.Chunk_File_Names[i]
+			go func() {
+				req, err := http.NewRequest("GET", url, nil)
+				if err != nil {
+					fmt.Println(err)
+				}
+				for key, value := range cookies {
+					req.AddCookie(&http.Cookie{Name: key, Value: value})
+				}
+				http_client := &http.Client{}
+				resp, err := http_client.Do(req)
+				if err != nil {
+					fmt.Println(err)
+				}
+				defer resp.Body.Close()
+				body, err := io.ReadAll(resp.Body)
+				if err != nil {
+					fmt.Println(err)
+				}
+				var chunk_channel_output ChunkResponseGrouping
+				chunk_channel_output.outer_index = outer_index
+				chunk_channel_output.customer_id = customer_id
+				chunk_channel_output.response = string(body)
+				chunk_channel <- chunk_channel_output
+			}()
+		}
+	}
+	output_strings := make([]string, len(customer_id_season_quarter_season_year_array_of_arrays))
+	for i := 0; i < channel_count; i++ {
+		chunk_channel_response := <-chunk_channel
+		outer_index := chunk_channel_response.outer_index
+		output_strings[outer_index] += (chunk_channel_response.response + ",")
+	}
+	for i := 0; i < len(customer_id_season_quarter_season_year_array_of_arrays); i++ {
+		value := customer_id_season_quarter_season_year_array_of_arrays[i][0]
+		customer_id := value.Customer_ID
+		file_name := strconv.Itoa(customer_id) + "-subsessions-output.json"
+		file, err := os.Create(file_name)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer file.Close()
+		_, err = file.Write([]byte("[" + output_strings[i] + "]"))
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
