2c77997a0306222c10dc89ab60232cf2278fa482
Shinsina <shinsina@att.net>
2024-08-22 10:32:47 -0500

    Adaptation of distinct-ids to support subsessions-output as input

--- utils/distinct-ids.py
+++ utils/distinct-ids.py
@@ -1,10 +1,10 @@
 import json
 
 subsessions = []
-with open('../5-1-2024-search-series-output.json', 'r') as input_file:
+with open('./subsessions-output.json', 'r') as input_file:
   json_data = json.load(input_file)
-  for key in json_data.keys():
-    subsessions.extend(json_data[key])
+  for value in json_data:
+    subsessions.append(value)
 
 series_ids = set()
 season_ids = set()
